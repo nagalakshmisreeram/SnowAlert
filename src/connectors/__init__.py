97f6ffe0e2438a7dac3bc23563bce843cdba4403
Greg Butzi <gregory.butzi@snowflake.com>
2019-07-18 12:17:25 -0700

    [CONNECTOR] Azure VM Inventory connector

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -1,6 +1,7 @@
 from . import aws_cloudtrail
 from . import okta
 from . import azure_log
+from . import azure_vm
 from . import aws_config
 from . import aws_asset_ingest
 
@@ -9,6 +10,7 @@ __all__ = [
     'aws_cloudtrail',
     'aws_config',
     'azure_log',
+    'azure_vm',
     'okta',
 ]
 
@@ -17,6 +19,7 @@ connectors = {
     'aws_cloudtrail': aws_cloudtrail,
     'aws_config': aws_config,
     'azure_log': azure_log,
+    'azure_vm': azure_vm,
     'okta': okta,
 }
 
